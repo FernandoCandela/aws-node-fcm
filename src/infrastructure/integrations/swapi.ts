624a246f112303b1851271b6e59141823c4e7276
FernandoCandela <20171937@aloe.ulima.edu.pe>
2024-02-11 12:16:15 -0500

    feat: avance starwars bd

--- src/infrastructure/integrations/swapi.ts
+++ src/infrastructure/integrations/swapi.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const SWAPI_BASE_URL: string = "https://swapi.py4e.com/api";
 
-export async function getPeopleById(id: number) {
+export async function getEntityById(entity: string, id: number) {
   try {
-    const response = await axios.get(`${SWAPI_BASE_URL}/people/${id}`);
+    const response = await axios.get(`${SWAPI_BASE_URL}/${entity}/${id}`);
     return response.data;
   } catch (error) {
     console.error("Error al obtener el personaje de SWAPI:", error);
